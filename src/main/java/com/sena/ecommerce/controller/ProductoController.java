3c7ee609da68a520d9f9375f17a1f137b2447b88
santiago <Santiagoeslava906@gmail.com>
2025-08-28 12:51:39 -0500

    v0.9_microservicio de imagenes modulo productos_ESLAVA_28082025

--- src/main/java/com/sena/ecommerce/controller/ProductoController.java
+++ src/main/java/com/sena/ecommerce/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.sena.ecommerce.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sena.ecommerce.model.Producto;
 import com.sena.ecommerce.model.Usuario;
 import com.sena.ecommerce.service.IProductoService;
+import com.sena.ecommerce.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -25,6 +29,9 @@ public class ProductoController {
 
 	@Autowired
 	private IProductoService productoservice;
+
+	@Autowired
+	private UploadFileService upload;
 
 	// metodo de listar productos
 	@GetMapping("")
@@ -41,10 +48,15 @@ public class ProductoController {
 
 	// metodo de creacion de productos
 	@PostMapping("/save")
-	public String save(Producto producto) {
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto del producto a guardar en la DB {}", producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
+		// validacion imagen del producto
+		if (producto.getId() == null) {
+			String nombreImagen = upload.saveImages(file, producto.getNombre());
+			producto.setImagen(nombreImagen);
+		}
 		productoservice.save(producto);
 		return "redirect:/productos";
 	}
@@ -62,10 +74,21 @@ public class ProductoController {
 	}
 
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto del producto a actualizar en la DB  {}", producto);
-		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
-		producto.setUsuario(u);
+		Producto p = new Producto();
+		p = productoservice.get(producto.getId()).get();
+		if (file.isEmpty()) {
+			producto.setImagen(p.getImagen());
+		} else {
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+
+			}
+			String nombreImagen = upload.saveImages(file, p.getNombre());
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoservice.update(producto);
 		return "redirect:/productos";
 	}
@@ -73,6 +96,11 @@ public class ProductoController {
 	// metodo para eliminar con id un producto
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		Producto p = new Producto();
+		p = productoservice.get(id).get();
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
 		productoservice.delete(id);
 		return "redirect:/productos";
 	}
